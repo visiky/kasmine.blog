5326acbd9add2a9744681d25062af0975f577ee4
kasmine <kasmine@kasminedeMacBook-Pro.local>
2017-09-19 17:40:35 +0800

    迁移我的个人笔记 - 添加可自动追加头文件的脚本

---
+++ bin/write_yml.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Function: append yml头部信息
+# Usage: ./bin/write_yml dir_name(包含没有头文件信息文件的文件夹)
+cd $1
+for file in `ls`
+do
+suffix=${file#*.*}  # 截取左边起第一个 . 之后的所有字符
+if [ ${suffix} == 'md' ]
+then
+fname=${file%.*} # % 从右边算起最后一个
+filetmp=$file.tmp%
+outerpara=1
+awk -v name="$fname" 'BEGIN{print "---\nlayout: post\ntitle: "name"\ntags:\n---\n"}{print $0}' $file > $filetmp
+mv $filetmp $file
+fi
+done
